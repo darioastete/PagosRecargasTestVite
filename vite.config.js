fc8800507f1d3b615cf50b1003e3480a09c69a29
userwinwsl <castete@synaptec.com>
2023-12-08 16:16:31 -0500

    flat: proxy add

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,19 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  // server: {
-  //   proxy: {
-  //     '/': {
-  //       target: 'http://bun-burn-env.eba-ftyx2m3h.us-east-1.elasticbeanstalk.com',
-  //       changeOrigin: true,
-  //     },
-  //   }
-  // }
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://bun-burn-env.eba-ftyx2m3h.us-east-1.elasticbeanstalk.com',
+        secure: false,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, PUT',
+          'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+        },
+      },
+    }
+  }
 })
