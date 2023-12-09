ac2aa33faf3e8caeee8266e6a0c70909be5e4381
userwinwsl <castete@synaptec.com>
2023-12-08 21:23:41 -0500

    feat: PWA added succesfull

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,26 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),
+   VitePWA({
+     srcDir: "/",
+     filename: "sw.js",
+     registerType: 'autoUpdate',
+     devOptions: {
+       enabled: true,
+       type: 'module',
+     },
+     strategies: "injectManifest",
+     injectRegister: false,
+     manifest: false,
+     injectManifest: {
+       injectionPoint: null,
+     },
+   }),
+  ],
   build: {
     minify: true,
     sourceMap: false
